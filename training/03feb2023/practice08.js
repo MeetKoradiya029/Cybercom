e53d95a7ab1dee0d434e4fa99090e312a65e4ef0
KoradiyaMeet029 <mvkoradiya1112@gmail.com>
2023-02-05 10:52:21 +0530

    practice08 completed

--- training/03feb2023/practice08.js
+++ training/03feb2023/practice08.js
@@ -3,6 +3,8 @@ function validate() {
   let re_email = document.getElementById("email02").value;
   let pass = document.getElementById("password").value;
   let re_pass = document.getElementById("password02").value;
+  
+
 
   var formData = [email,re_email,pass,re_pass];
 
@@ -29,9 +31,12 @@ function validate() {
   }
 
 
+
+
+
     localStorage.setItem("formData",JSON.stringify(formData));
 
-    var storedData = localStorage.getItem(JSON.stringify(formData));
+    var storedData = localStorage.getItem("formData");
 
     console.log(storedData);
 
@@ -43,6 +48,19 @@ function validate() {
 
 }
 
+function selector(){
+  let select_state = document.getElementById("state").value;
+  let cities = document.querySelectorAll("optgroup")
+
+  cities.forEach(element => {
+    if(element.id != select_state){
+      element.style.display="none"
+    }
+  });
+  
+
+  console.log(select_state);
+}
   
 
 
