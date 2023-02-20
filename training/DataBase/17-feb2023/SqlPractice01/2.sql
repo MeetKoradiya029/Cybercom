c61b20bed425af79d3cffda5967546faebaf34d4
KoradiyaMeet029 <mvkoradiya1112@gmail.com>
2023-02-20 10:58:42 +0530

    edited on 20feb2023

--- training/DataBase/17-feb2023/SqlPractice01/2.sql
+++ training/DataBase/17-feb2023/SqlPractice01/2.sql
@@ -6,16 +6,42 @@ student can be on one college only.
 
 CREATE TABLE colleges
 (
-	college_id INT,
+	college_id INT AUTO_INCREMENT,
     college_name VARCHAR(128),
     city VARCHAR(16),
     PRIMARY KEY (college_id)
 );
 CREATE TABLE students
 (
-	student_id INT,
+	student_id INT AUTO_INCREMENT,
     student_name VARCHAR(128),
-    college_id INT UNIQUE,
+    contact_no VARCHAR(32),
+    college_id INT,
     PRIMARY KEY (student_id),
-    FOREIGN KEY (college_id) REFERENCES colleges(college_id)
+    FOREIGN KEY (college_id) REFERENCES colleges(college_id) ON DELETE CASCADE
 );
+
+
+
+INSERT INTO colleges (college_name,city) 
+VALUES ("Gec BHavnagar","Bhavnagar");
+INSERT INTO colleges (college_name,city) 
+VALUES ("L.D.","Ahmedabad");
+INSERT INTO colleges (college_name,city) 
+VALUES ("Nirma University","Ahmedabad");
+
+
+INSERT INTO students (student_name,contact_no,college_id) 
+VALUES ("Meet Koradiya",'7284021006',1);
+INSERT INTO students (student_name,contact_no,college_id) 
+VALUES ("Meet Koradiya",'9173794282',1);
+INSERT INTO students (student_name,contact_no,college_id) 
+VALUES ("Raj Javiya",'8238949574',2);
+
+
+SELECT s.student_name from students as s 
+inner join c.
+
+
+
+
